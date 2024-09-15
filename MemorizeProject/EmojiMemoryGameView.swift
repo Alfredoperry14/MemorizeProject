a467d7a64d300a0ae2a76129c7184fa8cc9897a3
alfredoperry14 <alfredoperry14@gmail.com>
2024-09-14 17:13:36 -0700

    Lists no changes

--- MemorizeProject/EmojiMemoryGameView.swift
+++ MemorizeProject/EmojiMemoryGameView.swift
@@ -33,7 +33,6 @@ struct EmojiMemoryGameView: View {
                     .onTapGesture {
                         viewModel.choose(card)
                     }
-                Text(card.id)
             }
         }
         .foregroundColor(.orange)
